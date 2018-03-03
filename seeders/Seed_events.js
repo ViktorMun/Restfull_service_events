7f1bcd381971f79588cf2f2bf8fe30ed22d06fcc
ViktorMun <v.mun@yahoo.com>
2018-03-03 11:33:20 +0100

    Seed all to db

--- seeders/Seed_events.js
+++ seeders/Seed_events.js
@@ -4,6 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Allevents', [
       { title: 'New year', startdate: '2018-01-01', enddate: '2018-01-01', description: 'Magic', createdAt: 'NOW()', updatedAt: 'NOW()'  }
+
     ])
   },
 
